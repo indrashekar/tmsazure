c81ce132c1a2d184f097dd639cd8971da1378a4e
indrashekar <indrashekargr@gmail.com>
2017-11-17 18:48:41 +0530

    changes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var express = require('express'),
 var app = express();
 var port = 3000;
 var bodyParser = require('body-parser');
+
+var cors = require('cors')
+
 /*var busboy = require("then-busboy"),
 fileUpload = require('express-fileupload');*/
 
@@ -26,11 +29,23 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/app/index.html')));
 
+app.use(cors());
+
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-    next();
+    res.setHeader('Access-Control-Allow-Origin', 'http://tmpsapp.azurewebsites.net');
+    
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
 });
 
 app.use(bodyParser.json());
